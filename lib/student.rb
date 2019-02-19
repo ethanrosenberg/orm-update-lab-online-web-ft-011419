5594c14c3ff8e9038070a3b6cd5cf5da65ecd52f
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-19 20:21:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,9 +61,8 @@ class Student
       WHERE name = ?
     SQL
 
-    DB[:conn].execute(sql, name).map do |row|
-      self.new_from_db(row)
-    end.first
+    result = DB[:conn].execute(sql, name)[0]
+      Student.new(result[0], result[1], result[2])
   end
 
 
