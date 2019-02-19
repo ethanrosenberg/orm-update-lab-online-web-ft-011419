e8cd3c3e49c6a3c198edc52a89ddf9b5a80aad9b
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-02-19 20:24:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,7 +67,8 @@ class Student
     SQL
 
     result = DB[:conn].execute(sql, name)[0]
-      Student.new(result[0], result[1], result[2])
+    self.new_from_db(result)
+      #Student.new(result[0], result[1], result[2])
   end
 
   def update
